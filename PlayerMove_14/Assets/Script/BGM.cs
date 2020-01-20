bb4ed0fa13237d4fdc1959e43bcaf7cf5f6788d6
p156 <a>
2020-01-20 10:19:07 +0900

    BGM2

---
+++ PlayerMove_14/Assets/Script/BGM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BGM : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+            //画面遷移してもオブジェクトが壊れないようにする
+            DontDestroyOnLoad(this);
+    }
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "close")
+        {
+            audioSource.Stop();
+        }
+    }
+}
