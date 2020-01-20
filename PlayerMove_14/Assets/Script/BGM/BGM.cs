db9491d63950f655880e2214767ead5233e9a79e
p156 <a>
2020-01-21 05:06:44 +0900

    ステージ2　イベント

---
+++ PlayerMove_14/Assets/Script/BGM/BGM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGM : MonoBehaviour
+{
+    GameObject[] tagObjects;
+    public AudioSource audioSource;
+    //int objs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        tagObjects = GameObject.FindGameObjectsWithTag("BGM");
+        Destroy(tagObjects[1]);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (SceneManager.GetActiveScene().name == "stage2")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
