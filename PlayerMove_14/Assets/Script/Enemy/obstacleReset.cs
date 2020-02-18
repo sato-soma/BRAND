ef4301598a83cfd5c9a3a20ecffa4f5bdaff3162
s-soma <souma.1005@icloud.com>
2020-02-18 15:18:09 +0900

    敵の当たり判定消えてたから追加

--- PlayerMove_14/Assets/Script/Enemy/obstacleReset.cs
+++ PlayerMove_14/Assets/Script/Enemy/obstacleReset.cs
@@ -9,13 +9,16 @@ public class obstacleReset : MonoBehaviour
 
     public fallIcicles[] FallIcicles;
     public GameObject[] fallIcicless;
-    
+
+    public float[] time = new float[2];
 
     public int[] number = new int[2];
 
     // Start is called before the first frame update
     void Start()
     {
+        time[0] = time[1] = 0;
+
         for (int i = 0; i < number[0]; i++)
         {
             Bats[i] = bats[i].GetComponent<bat>();
@@ -35,21 +38,45 @@ public class obstacleReset : MonoBehaviour
             //コウモリ リスポーン
             for (int i = 0; i < number[0]; i++)
             {
-                Bats[i].pos = Bats[i].posSave;
-                Bats[i].bats.SetActive(true);
-                Bats[i].transform.position = Bats[i].pos;
+                if (time[0] < 5)
+                {
+                    time[0] += 1.0f / 60.0f;
+                }
+
+                if (time[0] > 5)
+                {
+                    Bats[i].pos = Bats[i].posSave;
+                    Bats[i].bats.SetActive(true);
+                    Bats[i].transform.position = Bats[i].pos;
+                    time[0] = -1;
+                }
+                
             }
 
             //石筍 リスポーン
             for (int j = 0; j < number[1]; j++)
             {
-                FallIcicles[j].pos = FallIcicles[j].posSave;
-                FallIcicles[j].hit = false;
-                FallIcicles[j].rigid.isKinematic = false;
-                FallIcicles[j].fallIcicless.SetActive(true);
-                FallIcicles[j].transform.position = FallIcicles[j].pos;
+
+                if (time[1] < 5)
+                {
+                    time[1] += 1.0f / 60.0f;
+                }
+
+                if (time[1] > 5)
+                {
+                    FallIcicles[j].pos = FallIcicles[j].posSave;
+                    FallIcicles[j].hit = false;
+                    FallIcicles[j].rigid.isKinematic = false;
+                    FallIcicles[j].fallIcicless.SetActive(true);
+                    FallIcicles[j].transform.position = FallIcicles[j].pos;
+                }
+
             }
             
+        }
+        else
+        {
+            time[0] = time[1] = 0;
         }
     }
 }
